1fcb66b6aa139b78edea68744ebb7bad378d8c7d
DibashBikramThapa <dwasthapa172@gmail.com>
2021-07-30 18:31:56 +0545

    back to sqlite3

--- portfolioproject/settings.py
+++ portfolioproject/settings.py
@@ -85,9 +85,9 @@ DATABASES = {
 }
 
 
-import dj_database_url
+#import dj_database_url
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
